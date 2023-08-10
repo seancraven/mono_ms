b6fb6c4d922180bbe6588ef83ee2422e5cb8736b
sean craven <seanpietercraven@gmail.com>
2023-08-10 17:33:07 +0100

    added descriptions

--- model_based/tests/test_catch.py
+++ model_based/tests/test_catch.py
@@ -32,13 +32,18 @@ def pretty_compare(a, b):
     if (a == b).all():
         return True
     else:
-        print()
-        print(a.reshape(10, 5))
-        print(b.reshape(10, 5))
+        # print()
+        # print(a.reshape(10, 5))
+        # print(b.reshape(10, 5))
         return False
 
 
-def test_model_equivariance():
+def test_model_equivariance_exclude_type1():
+    """Fails when model predictions are equivariant to
+    transformations on the input. This test excludes input states
+    that are along the center line and so
+    state @ group_action = state.
+    """
     model = tm.CatchEquiModel()
     key = jax.random.PRNGKey(10)
     state_gen = mock_catch(1000)
@@ -53,19 +58,50 @@ def test_model_equivariance():
         inv_action = tm.catch_action_transform(action)
         inv_obs = catch_transform(obs)
         if (inv_obs == obs).all():
-            print("Passing")
+            # Exclude states that the ball and paddle are
+            # in the middle column, 2, (zero index)
             continue
 
         inv_ball, inv_pad = apply_fn(model_params, inv_obs, inv_action)
-        inv_pred = model.dist_to_obs(inv_ball, inv_pad)
-        inv_pred = catch_transform(inv_pred).reshape(10, 5)
+        inv_pred = model.dist_to_obs(inv_ball, inv_pad).reshape(10, 5)
+        transform_pred = catch_transform(pred).reshape(10, 5)
 
-        # if not (inv_obs == obs).all():
-        comparison = pretty_compare(pred, inv_pred)
+        comparison = pretty_compare(transform_pred, inv_pred)
 
         if not comparison:
-            print()
+            # uncomment lines 35, 37 to see when goes wrong
+            # print()
             print(obs.reshape(10, 5))
         sol.append(comparison)
-    print("Fraction of test passed", sum(sol) / len(sol))
-    assert all(sol)
+    print("Fraction of Predictions that are equivarant", sum(sol) / len(sol))
+    assert all(sol), "Not all next state predictions are equivariant"
+
+
+def test_model_equivariance():
+    """Test that when the input state and input action are transformed,
+    the output state is transformed in the same manner.
+    checks for this property
+    group_action @ model(state) = model(group_action @ state).
+    """
+    model = tm.CatchEquiModel()
+    key = jax.random.PRNGKey(10)
+    state_gen = mock_catch(1000)
+    obs, action = next(state_gen)
+    model_params = model.init(key, obs, action)
+    apply_fn = jax.jit(model.apply)
+    sol = []
+    for obs, action in state_gen:
+        ball_dist, pad_dist = apply_fn(model_params, obs, action)
+        pred = model.dist_to_obs(ball_dist, pad_dist).reshape(10, 5)
+
+        inv_action = tm.catch_action_transform(action)
+        inv_obs = catch_transform(obs)
+        inv_ball, inv_pad = apply_fn(model_params, inv_obs, inv_action)
+        inv_pred = model.dist_to_obs(inv_ball, inv_pad).reshape(10, 5)
+        transform_pred = catch_transform(pred).reshape(10, 5)
+
+        comparison = pretty_compare(transform_pred, inv_pred)
+        sol.append(comparison)
+
+    print("Fraction of Predictions that are equivarant", sum(sol) / len(sol))
+    assert all(sol), "Not all next state predictions are equivariant"
